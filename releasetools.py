3bc246117e951dd9337cdfb396c20674984d7239
Caio Schnepper <caioschnepper@gmail.com>
2017-04-22 23:16:22 -0500

    Releasetools: resize system partition after flashing
    We need an enlarged system partition to fit even minimal gapps.
    Flash a stock sized partition and resize to the actual partition
    size before restoring gapps.
    
    Change-Id: Id6d0d89fab5249f8e6b7eae9c1dd365f0357afed

--- releasetools.py
+++ releasetools.py
@@ -12,65 +12,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Emit commands needed for Mantaray during OTA installation
-(installing the bootloader)."""
-
 import common
+import struct
 
-def FullOTA_InstallEnd(info):
-  try:
-    bootloader_img = info.input_zip.read("RADIO/bootloader.img")
-  except KeyError:
-    print "no bootloader.img in target_files; skipping install"
-  else:
-    WriteBootloader(info, bootloader_img)
+def FullOTA_PostValidate(info):
+  # run e2fsck
+  info.script.AppendExtra('run_program("/sbin/e2fsck", "-fy", "/dev/block/platform/dw_mmc.0/by-name/system");');
+  # resize2fs: run and delete
+  info.script.AppendExtra('run_program("/sbin/resize2fs", "/dev/block/platform/dw_mmc.0/by-name/system");');
+  # run e2fsck
+  info.script.AppendExtra('run_program("/sbin/e2fsck", "-fy", "/dev/block/platform/dw_mmc.0/by-name/system");');
 
-def IncrementalOTA_InstallBegin(info):
-  info.script.Unmount("/system")
-  info.script.TunePartition("/system", "-O", "^has_journal")
-  info.script.Mount("/system")
-
-def IncrementalOTA_VerifyEnd(info):
-  # try:
-  #   target_radio_img = info.target_zip.read("RADIO/radio.img")
-  #   source_radio_img = info.source_zip.read("RADIO/radio.img")
-  # except KeyError:
-  #   # No source or target radio. Nothing to verify
-  #   pass
-  # else:
-  #   if source_radio_img != target_radio_img:
-  #     info.script.CacheFreeSpaceCheck(len(source_radio_img))
-  #     radio_type, radio_device = common.GetTypeAndDevice("/radio", info.info_dict)
-  #     info.script.PatchCheck("%s:%s:%d:%s:%d:%s" % (
-  #         radio_type, radio_device,
-  #         len(source_radio_img), common.sha1(source_radio_img).hexdigest(),
-  #         len(target_radio_img), common.sha1(target_radio_img).hexdigest()))
-  pass
-
-def IncrementalOTA_InstallEnd(info):
-  try:
-    target_bootloader_img = info.target_zip.read("RADIO/bootloader.img")
-    try:
-      source_bootloader_img = info.source_zip.read("RADIO/bootloader.img")
-    except KeyError:
-      source_bootloader_img = None
-
-    if source_bootloader_img == target_bootloader_img:
-      print "bootloader unchanged; skipping"
-    else:
-      WriteBootloader(info, target_bootloader_img)
-  except KeyError:
-    print "no bootloader.img in target target_files; skipping install"
-
-def WriteBootloader(info, bootloader_img):
-  common.ZipWriteStr(info.output_zip, "bootloader.img", bootloader_img)
-  bl_type, bl_device = common.GetTypeAndDevice("/bootloader", info.info_dict)
-
-  fstab = info.info_dict["fstab"]
-
-  info.script.Print("Writing bootloader...")
-  force_ro = "/sys/block/" + bl_device.split("/")[-1] + "/force_ro"
-  info.script.AppendExtra(
-      ('samsung.manta.write_bootloader(package_extract_file('
-       '"bootloader.img"), "%s", "%s");') %
-      (bl_device, force_ro))
