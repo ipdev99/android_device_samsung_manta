5e0cf441071e5478f4098ea0bbcfb927a1f37183
followmsi <followmsi@gmail.com>
2017-05-11 09:06:08 +0200

    Wifi Display fixes Adding wpa_supplicant_overlay.conf & p2p_supplicant_overlay.conf

--- device.mk
+++ device.mk
@@ -103,6 +103,10 @@ PRODUCT_PACKAGES := \
     wpa_supplicant \
     wpa_supplicant.conf
 
+PRODUCT_COPY_FILES += \
+    device/samsung/manta/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
+    device/samsung/manta/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf
+
 # http://b/15193147
 # TODO(danalbert): Remove this once stlport is dead and gone.
 PRODUCT_PACKAGES +=  \
