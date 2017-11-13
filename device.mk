feb201a8a918504455f6f87faf1f56039984ee5a
followmsi <followmsi@gmail.com>
2017-11-13 18:35:43 +0100

    manta: enable binderized gralloc service

--- device.mk
+++ device.mk
@@ -161,6 +161,7 @@ DEVICE_PACKAGE_OVERLAYS := \
 # for now include gralloc here. should come from hardware/samsung_slsi/exynos5
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.renderscript@1.0-impl \
