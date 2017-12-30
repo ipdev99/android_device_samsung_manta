a6f6b56da4bac72bec02fff6e82fe6f254c5a1a5
Artem Borisov <dedsa2002@gmail.com>
2017-12-30 16:03:20 +0300

    manta: Enable boot and system server dex-preopt
    As stated in https://android.googlesource.com/platform/build/+/418258c
    this is required in O-MR1 since sepolicy now prevents system server from
    loading anything from /data making our system extremely slow and janky.
    
    Change-Id: I215ec7ebe95981ef0b953fe938e12b83675795ab

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -115,6 +115,16 @@ TARGET_BOOTANIMATION_USE_RGB565 := true
 # Seccomp filters
 BOARD_SECCOMP_POLICY += device/samsung/manta/seccomp
 
+# Dexpreopt
+ifeq ($(HOST_OS),linux)
+  ifneq ($(TARGET_BUILD_VARIANT),eng)
+    ifeq ($(WITH_DEXPREOPT),)
+      WITH_DEXPREOPT := true
+      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
+    endif
+  endif
+endif
+
 #TWRP
 #TARGET_PREBUILT_KERNEL := device/samsung/manta/kernel
 #TARGET_RECOVERY_UI_LIB := librecovery_ui_manta
