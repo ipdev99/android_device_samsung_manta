7765572af78fefecb651fb78b1bee2684f6c8344
followmsi <followmsi@gmail.com>
2018-04-05 23:00:10 +0200

    manta: set build fingerprint instead of override

--- rr.mk
+++ rr.mk
@@ -21,5 +21,6 @@ PRODUCT_MANUFACTURER := samsung
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=mantaray \
-    BUILD_FINGERPRINT=google/mantaray/manta:5.1.1/LMY49J/2640980:user/release-keys \
     PRIVATE_BUILD_DESC="mantaray-user 5.1.1 LMY49J 2640980 release-keys"
+
+BUILD_FINGERPRINT := google/mantaray/manta:5.1.1/LMY49J/2640980:user/release-keys
