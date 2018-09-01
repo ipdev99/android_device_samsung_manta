025ee6b2d822aac7d599ad9ef0a97598cb4be9a1
followmsi <followmsi@gmail.com>
2018-09-01 14:07:26 +0200

    manta: set build fingerprint instead of override

--- full_manta.mk
+++ full_manta.mk
@@ -46,6 +46,6 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=mantaray \
-    BUILD_FINGERPRINT=google/mantaray/manta:5.1.1/LMY49J/2640980:user/release-keys \
     PRIVATE_BUILD_DESC="mantaray-user 5.1.1 LMY49J 2640980 release-keys"
 
+BUILD_FINGERPRINT := google/mantaray/manta:5.1.1/LMY49J/2640980:user/release-keys
