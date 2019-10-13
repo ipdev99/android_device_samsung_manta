144291414b839c539dac0fc2ec261ef898485897
followmsi <followmsi@gmail.com>
2019-10-13 15:52:58 +0200

    manta: Add android.hardware.power@1.0-service

--- device.mk
+++ device.mk
@@ -201,6 +201,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.power@1.0-impl \
+    android.hardware.power@1.0-service \
     power.manta
 
 PRODUCT_PROPERTY_OVERRIDES := \
