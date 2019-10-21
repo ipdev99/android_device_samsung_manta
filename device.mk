f007e54b47de7d20896a63ecb6efa51cef9ce271
followmsi <followmsi@gmail.com>
2019-10-21 15:11:50 +0200

    Revert "manta: Disable GPS (TEMP)"
    This reverts commit 8ddd61e022f4267c028a32a6c936dd5423d62457.

--- device.mk
+++ device.mk
@@ -127,11 +127,11 @@ PRODUCT_COPY_FILES += \
     device/samsung/manta/p2p_supplicant_overlay.conf:system/vendor/etc/wifi/p2p_supplicant_overlay.conf
 
 # GNSS HAL
-#PRODUCT_PACKAGES += \
-#    android.hardware.gnss@1.0-impl
-#
-#PRODUCT_PACKAGES += \
-#    gpsd_shim
+PRODUCT_PACKAGES += \
+    android.hardware.gnss@1.0-impl
+
+PRODUCT_PACKAGES += \
+    gpsd_shim
 
 # USB HAL
 PRODUCT_PACKAGES += \
@@ -154,11 +154,11 @@ PRODUCT_PACKAGES += \
     android.hardware.health@1.0-impl
 
 # BCM47511 GPS
-#PRODUCT_COPY_FILES += \
-#    device/samsung/manta/gps/gps.conf:system/etc/gps.conf \
-#    device/samsung/manta/gps/gpsd:system/vendor/bin/gpsd \
-#    device/samsung/manta/gps/gps.xml:system/vendor/etc/gps.xml \
-#    device/samsung/manta/gps/gps.manta.so:system/lib/hw/gps.manta.so
+PRODUCT_COPY_FILES += \
+    device/samsung/manta/gps/gps.conf:system/etc/gps.conf \
+    device/samsung/manta/gps/gpsd:system/vendor/bin/gpsd \
+    device/samsung/manta/gps/gps.xml:system/vendor/etc/gps.xml \
+    device/samsung/manta/gps/gps.manta.so:system/lib/hw/gps.manta.so
 
 # NFC packages
 #PRODUCT_PACKAGES += \
