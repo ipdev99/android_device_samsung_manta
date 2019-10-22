5a25069a8571636df44f3f7fc013188d4f42b55d
followmsi <followmsi@gmail.com>
2019-10-22 15:10:02 +0200

    move gpsd to /vendor/bin/hw/

--- device.mk
+++ device.mk
@@ -155,7 +155,7 @@ PRODUCT_PACKAGES += \
 # BCM47511 GPS
 PRODUCT_COPY_FILES += \
     device/samsung/manta/gps/gps.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gps.conf \
-    device/samsung/manta/gps/gpsd:$(TARGET_COPY_OUT_VENDOR)/bin/gpsd \
+    device/samsung/manta/gps/gpsd:$(TARGET_COPY_OUT_VENDOR)/bin/hw/gpsd \
     device/samsung/manta/gps/gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/gps.xml \
     device/samsung/manta/gps/gps.manta.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/gps.manta.so
 
